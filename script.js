f22ab3eb03b839976d4fbf4a86e74541705ea53b
laura-farias-dev <lmfs@discente.ifpe.edu.br>
2024-06-01 16:53:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const music = document.getElementById('music');
+    const playPauseBtn = document.getElementById('play-pause-btn');
+    const volumeControl = document.getElementById('volume');
+
+    playPauseBtn.addEventListener('click', function() {
+        if (music.paused) {
+            music.play();
+            playPauseBtn.innerHTML = '<i class="fas fa-heart"></i>';
+        } else {
+            music.pause();
+            playPauseBtn.innerHTML = '<i class="fas fa-heart-broken"></i>';
+        }
+    });
+
+    volumeControl.addEventListener('input', function() {
+        music.volume = volumeControl.value;
+    });
+
+    music.loop = true; // Adicionando loop para a música
+});
